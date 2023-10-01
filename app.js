80fb141f9b7a8aa67ea8a69e764fed9fb09c32d1
SudhanshuSingh21 <95579213+SudhanshuSingh21@users.noreply.github.com>
2023-10-01 21:29:16 +0530

    MongoDB cluster connection made

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Nostrud ea ex minim ad nulla. Consequat laboris consequat dolor deserunt exercitation est esse adipisicing aute cupidatat ut elit. Officia dolore mollit ex enim id pariatur proident ut id irure. Sit velit enim fugiat irure occaecat consectetur tempor sint elit elit consectetur ullamco consectetur. Laboris mollit tempor qui aliqua aliquip sint in amet magna reprehenderit laborum labore. Lorem fugiat exercitation cupidatat laborum culpa est deserunt sit minim."
+const homeStartingContent = "Blogging is as simple as obtaining a website and publishing original content on it. Tech-savvy bloggers can buy a domain name and build the website themselves. Those with less HTML knowledge can create an account with sites like WordPress that simplify the web design and publishing process." + "Blogs are usually simple websites. Older pieces may be archived in separate sections of the site, and there may be a separate page with contact info or a bio, but the blog itself is usually just a single page that can be scrolled through—similar to the news feed on social media sites like Facebook. As with a Facebook news feed, a blog displays the newest content at the top of the page.";
 const aboutContent = "loremDuis velit qui mollit ut dolor non non magna excepteur enim minim. Voluptate reprehenderit deserunt tempor laborum consequat qui ut labore deserunt non voluptate eiusmod tempor. Ut duis velit cillum magna amet proident. Eiusmod officia nostrud tempor cillum eu consectetur do non quis ad anim in."
 const contactContent = "Velit id laborum excepteur elit eiusmod ex. Reprehenderit veniam officia consequat velit exercitation ut non tempor. Ea nostrud amet eu eiusmod anim anim minim ea duis et."
 
-mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://admin-sudhanshu:Test123@cluster0.apallrb.mongodb.net/blogDB")
+        .catch(function (error) {
+            console.log(`Unable to connect to the Mongo db  ${error} `);
+        });
+}
+
 
 const postSchema = {
     title: String,
@@ -77,6 +84,7 @@ app.post("/compose", function (req, res) {
 });
 
 
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
